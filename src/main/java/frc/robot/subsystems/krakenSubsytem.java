94aaff960b6974d05dd511c739ad61a7d646670d
Bassem Şerifoğlu <153466589+Bassem-Serifoglu@users.noreply.github.com>
2025-12-28 21:40:46 +0300

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/krakenSubsytem.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class krakenSubsytem extends SubsystemBase {
+  TalonFX kraken = new TalonFX(10); 
+  VoltageOut vOut = new VoltageOut(0);
+
+  
+  /** Creates a new krakenSubsytem. */
+  public krakenSubsytem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setVoltage(double volts){
+    kraken.setControl(vOut.withOutput(volts));
+  }
+}
